8132df9b46a5a2634fe534c0eb77e2c4aeec7c87
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-28 13:07:09 +0300

    Index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 // // Write your solution in this file!
-// let employee = {
-//     name: 'Sydney Wendoe',
-//     streetAddress: '123 Main St'
-//   };
 
-//   // Fn to update employee w/out mutating the original obj;
+const employee = {
+    name: 'Sydney Wendoe',
+    streetAddress: '123 Main St'
+};
 
-// function updateEmployeeWithKeyAndValue(employee, key, value){
-//    return{ ...employee,     //Used spread operator to !mutate employee
-//     [key]: value
-// };
-// }
-
-// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-//   employee[key] = value;
-//   return employee;
-// }
-
-// function deleteFromEmployeeByKey(employee, key) {
-//     const newEmployee = { ...employee };
-//     delete newEmployee[key];
-//     return newEmployee;
-//   }
-
-//   function destructivelyDeleteFromEmployeeByKey(employee, key) {
-//     delete employee[key];
-//     return employee;
-//   }
-
-let employee = {
-    name: "Walter",
-    streetAddress: "00123 Ayoro",
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
 }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = { ...employee };
+    delete newEmployee[key]; //by key means we delete the entire key
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
